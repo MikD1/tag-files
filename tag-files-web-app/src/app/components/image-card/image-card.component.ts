dd4eccafa38fa4504ad1a73a8378610ccad3864f
Mikhail Doshevsky <m.doshevsky@gmail.com>
2025-04-21 23:46:27 +0300

    Minor fixes

--- tag-files-web-app/src/app/components/image-card/image-card.component.ts
+++ tag-files-web-app/src/app/components/image-card/image-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { FileMetadata } from '../../model/file-metadata';
