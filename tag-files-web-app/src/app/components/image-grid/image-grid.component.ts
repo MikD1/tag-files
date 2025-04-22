ffac1cdf9d3a799f4d30dff88a70b911aa7aced1
Mikhail Doshevsky <m.doshevsky@gmail.com>
2025-04-23 02:58:04 +0300

    Implement gallery

--- tag-files-web-app/src/app/components/image-grid/image-grid.component.ts
+++ tag-files-web-app/src/app/components/image-grid/image-grid.component.ts
@@ -1,14 +1,16 @@
 import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { ImageCardComponent } from "../image-card/image-card.component";
 import { AppStateService } from '../../services/app-state.service';
 import { LibraryItemPaginatedList, LibraryService } from '../../services/library.service';
 import { Observable } from 'rxjs';
+import { LightgalleryModule } from 'lightgallery/angular';
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgRotate from 'lightgallery/plugins/rotate';
 
 @Component({
   selector: 'app-image-grid',
-  imports: [MatGridListModule, ImageCardComponent, AsyncPipe],
+  imports: [MatGridListModule, AsyncPipe, LightgalleryModule],
   templateUrl: './image-grid.component.html',
   styleUrl: './image-grid.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,6 +21,15 @@ export class ImageGridComponent {
   }
 
   protected libraryItemsList: Observable<LibraryItemPaginatedList>;
+
+  protected gallerySettings = {
+    plugins: [lgZoom, lgRotate],
+    download: false,
+    counter: false,
+    flipHorizontal: false,
+    flipVertical: false,
+    rotateLeft: false
+  };
 
   protected readonly getGridColumns = computed(() => {
     return this.appStateService.getMainGridColumns();
