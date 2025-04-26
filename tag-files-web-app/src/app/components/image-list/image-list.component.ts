0f385b30f7357b29d7c62760addb9f650aece0c9
Mikhail Doshevsky <m.doshevsky@gmail.com>
2025-04-27 07:24:26 +1000

    Implement gallery list

--- tag-files-web-app/src/app/components/image-list/image-list.component.ts
+++ tag-files-web-app/src/app/components/image-list/image-list.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LibraryItemPaginatedList, LibraryService } from '../../services/library.service';
+import { AppStateService } from '../../services/app-state.service';
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgRotate from 'lightgallery/plugins/rotate';
+import { AsyncPipe } from '@angular/common';
+import { LightgalleryModule } from 'lightgallery/angular';
+import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-image-list',
-  imports: [],
+  imports: [AsyncPipe, LightgalleryModule, MatTableModule, MatChipsModule],
   templateUrl: './image-list.component.html',
   styleUrl: './image-list.component.scss'
 })
 export class ImageListComponent {
+  constructor() {
+    this.libraryItemsList = this.libraryService.search({ tagQuery: "", pageIndex: 1, pageSize: 100 })
+  }
 
+  protected gallerySettings = {
+    selector: ".gallery-item",
+    plugins: [lgZoom, lgRotate],
+    download: false,
+    counter: false,
+    flipHorizontal: false,
+    flipVertical: false,
+    rotateLeft: false
+  };
+
+  protected libraryItemsList: Observable<LibraryItemPaginatedList>;
+  protected displayedColumns = ['image', 'tags', 'uploadedOn'];
+
+  protected readonly getThumbnailSize = computed(() => {
+    const level = this.appStateService.getGalleryThumbnailSize();
+    return level * 16;
+  })
+
+  private readonly appStateService = inject(AppStateService);
+  private readonly libraryService = inject(LibraryService);
 }
