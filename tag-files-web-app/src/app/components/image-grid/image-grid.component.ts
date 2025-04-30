b9ce36c3c0d752f3570eba91a52422c4eaec0075
Балов Антон Алексеевич <a.balov@tbank.ru>
2025-05-01 02:31:56 +0300

    add video support

--- tag-files-web-app/src/app/components/image-grid/image-grid.component.ts
+++ tag-files-web-app/src/app/components/image-grid/image-grid.component.ts
@@ -4,6 +4,7 @@ import {AppStateService} from '../../services/app-state.service';
 import {LightgalleryModule} from 'lightgallery/angular';
 import lgZoom from 'lightgallery/plugins/zoom';
 import lgRotate from 'lightgallery/plugins/rotate';
+import lgVideo from 'lightgallery/plugins/video';
 import {SearchService} from '../../services/search.service';
 
 @Component({
@@ -15,7 +16,7 @@ import {SearchService} from '../../services/search.service';
 })
 export class ImageGridComponent {
   protected gallerySettings = {
-    plugins: [lgZoom, lgRotate],
+    plugins: [lgVideo, lgZoom, lgRotate],
     download: false,
     counter: false,
     flipHorizontal: false,
@@ -32,4 +33,24 @@ export class ImageGridComponent {
     const level = this.appStateService.getGalleryThumbnailSize();
     return 4 + (max - level);
   })
+
+  isVideo(path: string): boolean {
+    return path.endsWith('mp4')
+  }
+
+  getVideoData(path: string): string {
+    return JSON.stringify({
+      source: [
+        {
+          src: `http://localhost:5010/${path}`,
+          type: 'video/mp4',
+        }
+      ],
+      attributes: {
+        preload: false,
+        playsinline: true,
+        controls: true
+      }
+    });
+  }
 }
