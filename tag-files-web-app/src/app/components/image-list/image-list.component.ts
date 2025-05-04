823bed3056f9069b0b7422c3626537359c39f447
Mikhail Doshevsky <m.doshevsky@gmail.com>
2025-05-04 23:02:44 +0300

    Add edit button to library page

--- tag-files-web-app/src/app/components/image-list/image-list.component.ts
+++ tag-files-web-app/src/app/components/image-list/image-list.component.ts
@@ -1,23 +1,28 @@
-import {Component, input} from '@angular/core';
+import {Component, inject, input} from '@angular/core';
 import {LightgalleryModule} from 'lightgallery/angular';
 import {MatTableModule} from '@angular/material/table';
 import {MatChipsModule} from '@angular/material/chips';
 import {LightGallerySettings} from 'lightgallery/lg-settings';
 import {FileType, LibraryItem, LibraryItemPaginatedList} from '../../services/api/library-api.service';
+import {Router} from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 const ContentBaseUrl = "http://localhost:5010/"; // TODO: Move to config
 
 @Component({
   selector: 'app-image-list',
-  imports: [LightgalleryModule, MatTableModule, MatChipsModule],
+  imports: [LightgalleryModule, MatTableModule, MatChipsModule, MatIconModule, MatButtonModule],
   templateUrl: './image-list.component.html',
   styleUrl: './image-list.component.scss'
 })
 export class ImageListComponent {
   gallerySettings = input.required<LightGallerySettings>();
   itemsList = input.required<LibraryItemPaginatedList>();
+  protected router = inject(Router);
   protected fileTypes = FileType;
-  protected displayedColumns = ['image', 'tags', 'uploadedOn'];
+  protected displayedColumns = ['image', 'tags', 'uploadedOn', 'actions'];
 
   protected getFullThumbnailPath(thumbnailPath?: string): string {
     if (!thumbnailPath) {
@@ -45,5 +50,9 @@ export class ImageListComponent {
         controls: true
       }
     });
+  }
+
+  protected editItem(id: number) {
+    this.router.navigate(['/library/edit', id]);
   }
 }
