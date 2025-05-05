7d1eca0e80b7bd9aac4375c50aea4c781503f943
Anton <74132592+AntonRls@users.noreply.github.com>
2025-05-06 00:18:22 +0300

    update docker

---
+++ tag-files-service/TagFilesService.WebHost/Options/S3Option.cs
@@ -0,0 +1,10 @@
+namespace TagFilesService.WebHost.Options;
+
+public class S3Option
+{
+    public string? Host { get; set; }
+    
+    public string? AccessKey { get; set; }
+    
+    public string? SecretKey { get; set; }
+}
