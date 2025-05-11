63d18257e5c7bae1b47f2aef2528c39f7b9b088a
Mikhail Doshevsky <m.doshevsky@gmail.com>
2025-05-12 00:54:06 +0300

    Save app state to local storage

--- tag-files-web-app/src/app/services/app-state.service.ts
+++ tag-files-web-app/src/app/services/app-state.service.ts
@@ -3,17 +3,23 @@ import {Injectable, signal} from '@angular/core';
 const maxGalleryThumbnailSize = 7; // TODO: configure in settings
 
 export enum GalleryViewType {
-  Grid,
-  List
+  Grid = 'grid',
+  List = 'list'
 }
+
+const localStorageKeys = {
+  isSidebarOpen: 'appState.isSidebarOpen',
+  galleryThumbnailSize: 'appState.galleryThumbnailSize',
+  galleryViewType: 'appState.galleryViewType',
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppStateService {
-  private readonly isSidebarOpen = signal<boolean>(true);
-  private readonly galleryThumbnailSize = signal<number>(3);
-  private readonly galleryViewType = signal<GalleryViewType>(GalleryViewType.Grid);
+  private readonly isSidebarOpen = signal<boolean>(this.loadFromLocalStorage(localStorageKeys.isSidebarOpen, true));
+  private readonly galleryThumbnailSize = signal<number>(this.loadFromLocalStorage(localStorageKeys.galleryThumbnailSize, 3));
+  private readonly galleryViewType = signal<GalleryViewType>(this.loadFromLocalStorage(localStorageKeys.galleryViewType, GalleryViewType.Grid));
 
   getIsSidebarOpen() {
     return this.isSidebarOpen()
@@ -28,22 +34,49 @@ export class AppStateService {
   }
 
   toggleSidebar() {
-    this.isSidebarOpen.update(prev => !prev);
+    this.isSidebarOpen.update(prev => {
+      const newValue = !prev;
+      this.saveToLocalStorage(localStorageKeys.isSidebarOpen, newValue);
+      return newValue;
+    });
   }
 
   increaseMainGridColumns() {
-    this.galleryThumbnailSize.update(prev => prev < maxGalleryThumbnailSize ? prev + 1 : prev);
+    this.galleryThumbnailSize.update(prev => {
+      const newValue = prev < maxGalleryThumbnailSize ? prev + 1 : prev;
+      this.saveToLocalStorage(localStorageKeys.galleryThumbnailSize, newValue);
+      return newValue;
+    });
   }
 
   decreaseMainGridColumns() {
-    this.galleryThumbnailSize.update(prev => prev > 1 ? prev - 1 : prev);
+    this.galleryThumbnailSize.update(prev => {
+      const newValue = prev > 1 ? prev - 1 : prev;
+      this.saveToLocalStorage(localStorageKeys.galleryThumbnailSize, newValue);
+      return newValue;
+    });
   }
 
   setGridGalleryView() {
-    this.galleryViewType.update(() => GalleryViewType.Grid);
+    this.galleryViewType.update(() => {
+      this.saveToLocalStorage(localStorageKeys.galleryViewType, GalleryViewType.Grid);
+      return GalleryViewType.Grid;
+    });
   }
 
   setListGalleryView() {
-    this.galleryViewType.update(() => GalleryViewType.List);
+    this.galleryViewType.update(() => {
+      this.saveToLocalStorage(localStorageKeys.galleryViewType, GalleryViewType.List);
+      return GalleryViewType.List;
+    });
+  }
+
+  private saveToLocalStorage<T>(key: string, value: T): void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  private loadFromLocalStorage<T>(key: string, defaultValue: T): T {
+    const storedValue = localStorage.getItem(key);
+    return storedValue !== null ? JSON.parse(storedValue) : defaultValue;
   }
 }
