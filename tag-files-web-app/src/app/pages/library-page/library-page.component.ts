bf9817b17ab19d5e147dddad99dcfc8df7544bb3
Mikhail Doshevsky <m.doshevsky@gmail.com>
2025-05-18 21:16:21 +0300

    Add video control by keyboard

--- tag-files-web-app/src/app/pages/library-page/library-page.component.ts
+++ tag-files-web-app/src/app/pages/library-page/library-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, inject} from '@angular/core';
+import {Component, computed, HostListener, inject} from '@angular/core';
 import {ImageGridComponent} from '../../components/image-grid/image-grid.component';
 import {AppStateService, GalleryViewType} from '../../services/app-state.service';
 import {ImageListComponent} from '../../components/image-list/image-list.component';
@@ -21,9 +21,10 @@ export class LibraryPageComponent {
     counter: false,
     flipHorizontal: false,
     flipVertical: false,
-    rotateLeft: false
+    rotateLeft: false,
+    keyPress: false,
+    gotoNextSlideOnVideoEnd: false
   };
-
   protected galleryViewTypes = GalleryViewType;
   private readonly appStateService = inject(AppStateService);
   protected readonly galleryViewType = computed<GalleryViewType>(() => {
@@ -33,4 +34,32 @@ export class LibraryPageComponent {
   protected readonly searchResults = computed(() => {
     return this.searchService.searchResults()
   });
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent): void {
+    const videoElement = document.querySelector('video');
+    if (videoElement) {
+      switch (event.code) {
+        case 'Space':
+          if (videoElement.paused) {
+            videoElement.play();
+          } else {
+            videoElement.pause();
+          }
+          break;
+        case 'ArrowRight':
+          videoElement.currentTime += 4;
+          break;
+        case 'ArrowLeft':
+          videoElement.currentTime -= 2;
+          break;
+        case 'ArrowUp':
+          videoElement.volume = Math.min(videoElement.volume + 0.1, 1);
+          break;
+        case 'ArrowDown':
+          videoElement.volume = Math.max(videoElement.volume - 0.1, 0);
+          break;
+      }
+    }
+  }
 }
