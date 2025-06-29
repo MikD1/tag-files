2a36b61b2e64de37808922dc5ba0f9037cde9e47
Mikhail Doshevsky <m.doshevsky@gmail.com>
2025-06-29 15:18:42 +0300

    Add content page

--- tag-files-web-app/src/app/app.routes.ts
+++ tag-files-web-app/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router';
 import {LibraryPageComponent} from './pages/library-page/library-page.component';
 import {TagsPageComponent} from './pages/tags-page/tags-page.component';
 import {CategoriesPageComponent} from './pages/categories-page/categories-page.component';
+import {ContentPageComponent} from './pages/content-page/content-page.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/library', pathMatch: 'full'},
   {path: 'library', component: LibraryPageComponent},
   {path: 'tags', component: TagsPageComponent},
   {path: 'categories', component: CategoriesPageComponent},
+  {path: 'content/:id', component: ContentPageComponent},
 ];
