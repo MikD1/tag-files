455ae06545c6ac7e3a57df824b2f98b16685e391
Mikhail Doshevsky <m.doshevsky@gmail.com>
2025-09-30 01:48:28 +0300

    Add migrations

---
+++ tag-files-service/TagFilesService.Infrastructure/Migrations/20250929211941_ChangeVideoDurationType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TagFilesService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeVideoDurationType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "VideoDuration",
+                table: "LibraryItems",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "VideoDuration",
+                table: "LibraryItems",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "INTEGER",
+                oldNullable: true);
+        }
+    }
+}
---
+++ tag-files-service/TagFilesService.Infrastructure/Migrations/20250929214538_FixVideoDurationDataConversion.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TagFilesService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixVideoDurationDataConversion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE LibraryItems
+                SET VideoDuration =
+                    (
+                        /* whole seconds -> ticks */
+                        (
+                            CAST(SUBSTR(VideoDuration,1,2) AS INTEGER) * 3600 +
+                            CAST(SUBSTR(VideoDuration,4,2) AS INTEGER) * 60  +
+                            CAST(SUBSTR(VideoDuration,7,2) AS INTEGER)
+                        ) * 10000000
+                    )
+                    +
+                    /* fractional part -> ticks (pad/truncate to 7 digits) */
+                    CASE
+                        WHEN INSTR(VideoDuration, '.') > 0 THEN
+                            CAST(
+                                SUBSTR(
+                                    (SUBSTR(VideoDuration, INSTR(VideoDuration, '.') + 1) || '0000000'),
+                                    1, 7
+                                ) AS INTEGER
+                            )
+                        ELSE 0
+                    END
+                WHERE typeof(VideoDuration) = 'text'
+                      AND VideoDuration IS NOT NULL;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
